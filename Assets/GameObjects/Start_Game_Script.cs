1247a687e29198a2fba6ba93c586b6ebfdce1c85
nickpapenheim <93259475+nickpapenheim@users.noreply.github.com>
2026-04-24 20:47:17 +0200

    test

---
+++ Assets/GameObjects/Start_Game_Script.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Start_Game_Script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void start_game() {
+        
+    }
+}
